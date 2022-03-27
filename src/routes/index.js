52c0853cce9924b66a25adb68b897d2a528230c6
David Almeida <contato.davidalmeida@outlook.com>
2022-03-27 17:40:41 -0300

    ✅ Add: Creating routes file

---
+++ src/routes/index.js
@@ -0,0 +1,15 @@
+import { Switch, Route } from "react-router-dom";
+import Homepage from "../pages/Homepage";
+
+
+const Routes = () => {
+  return (
+    <Switch>
+      <Route exact path="/">
+        <Homepage />
+      </Route>
+    </Switch>
+  )
+}
+
+export default Routes;
